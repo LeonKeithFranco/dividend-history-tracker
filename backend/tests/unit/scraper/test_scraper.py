7942e4aff5affd96a5380877a6f0126ead56e9e5
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-04-23 14:57:06 -0700

    add current progress on tests

---
+++ backend/tests/unit/scraper/test_scraper.py
@@ -0,0 +1,68 @@
+from decimal import ROUND_HALF_UP, Decimal
+
+import pytest
+
+from scraper.scraper import get_dividend_info
+
+
+class TestScraper:
+    @pytest.mark.live
+    def test_scrape_ko_ticker_live(self):
+        """Tests the live version of the dividendhistory.org page for the Coca Cola
+            company.
+
+        This test was written on 2026-04-25 but assumes that Coca Cola will be an
+        on-going concern, and therefore the test is written to try and account for
+        future dividend events.
+        """
+        stock_info, dividend_metrics, dividend_history = get_dividend_info("KO")
+
+        assert stock_info.company_name == "Coca Cola"
+        assert stock_info.ticker_symbol == "KO"
+        assert stock_info.exchange == "NYSE"
+
+        assert dividend_metrics.yield_ > 0
+        assert dividend_metrics.pay_out_ratio > 0
+        assert dividend_metrics.frequency == "Quarterly"
+        assert (
+            dividend_metrics.next_payout_date >= dividend_metrics.next_ex_dividend_date
+        )
+
+        # 257 is the current number of dividend events at time of writing this test
+        assert len(dividend_history.dividend_events) >= 257
+
+        for i in range(1, len(dividend_history.dividend_events)):
+            # verify events are in reverse chronological order
+            assert (
+                dividend_history.dividend_events[i - 1].ex_dividend_date
+                > dividend_history.dividend_events[i].ex_dividend_date
+            )
+
+        for div_event in dividend_history.dividend_events:
+            assert div_event.payout_date >= div_event.ex_dividend_date
+
+        for i, div_event in enumerate(dividend_history.dividend_events):
+            if div_event.pct_change is not None:
+                current_dividend = div_event.cash_amount
+                previous_divdend = dividend_history.dividend_events[i + 1].cash_amount
+                calculated_pct_change = (
+                    (current_dividend / previous_divdend - 1) * 100
+                ).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+
+                assert Decimal(str(div_event.pct_change)) == calculated_pct_change
+
+        assert (
+            dividend_metrics.annual_dividend
+            == dividend_history.dividend_events[0].cash_amount * 4
+        )
+        assert (
+            dividend_metrics.next_ex_dividend_date
+            == dividend_history.dividend_events[1].ex_dividend_date
+        )
+        assert (
+            dividend_metrics.next_payout_date
+            == dividend_history.dividend_events[1].payout_date
+        )
+
+    def test_scrape_aapl_ticker_offline(self):
+        """Tests a downloaded version of the AAPL page on dividendhistory.org."""
