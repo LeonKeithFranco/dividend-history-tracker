c1e921dab577e48fe8835260f39bdf1b429eeca1
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-04-23 17:56:47 -0700

    add tests for scraper

--- backend/tests/unit/scraper/test_scraper.py
+++ backend/tests/unit/scraper/test_scraper.py
@@ -1,8 +1,33 @@
+import json
+from datetime import date, datetime
 from decimal import ROUND_HALF_UP, Decimal
+from pathlib import Path
 
 import pytest
+from pytest_mock import MockFixture
 
-from scraper.scraper import get_dividend_info
+from scraper.scraper import DividendEvent, DividendHistory, get_dividend_info
+
+
+@pytest.fixture
+def _aapl_dividend_history_from_json() -> DividendHistory:
+    with open(
+        Path(__file__).parent.parent.parent / "fixtures" / "aapl_dividend_history.json",
+        "r",
+    ) as f:
+        data = json.load(f)
+
+    events = [
+        DividendEvent(
+            ex_dividend_date=date.fromisoformat(event["ex_dividend_date"]),
+            payout_date=date.fromisoformat(event["payout_date"]),
+            cash_amount=Decimal(event["cash_amount"]),
+            pct_change=event["pct_change"],
+        )
+        for event in data["dividend_history"]["dividend_events"]
+    ]
+
+    return DividendHistory(dividend_events=events)
 
 
 class TestScraper:
@@ -64,5 +89,51 @@ class TestScraper:
             == dividend_history.dividend_events[1].payout_date
         )
 
-    def test_scrape_aapl_ticker_offline(self):
-        """Tests a downloaded version of the AAPL page on dividendhistory.org."""
+    def test_scrape_aapl_ticker_offline(
+        self,
+        mocker: MockFixture,
+        local_web_server: str,
+        _aapl_dividend_history_from_json: DividendHistory,
+    ):
+        """Tests a downloaded version of the AAPL page on dividendhistory.org.
+
+        This test uses mocking to redirect to the local doanloaded version of the APPL
+        page.
+        """
+        from scraper.selenium_wrapper import SeleniumWrapper
+
+        original_open_page = SeleniumWrapper.open_page
+
+        def modified_open_page(self, url):
+            original_open_page(self, local_web_server)
+
+        mocker.patch.object(
+            SeleniumWrapper,
+            "open_page",
+            new=modified_open_page,
+        )
+
+        stock_info, dividend_metrics, dividend_history = get_dividend_info("AAPL")
+
+        assert stock_info.ticker_symbol == "AAPL"
+        assert stock_info.company_name == "Apple"
+        assert stock_info.exchange == "Nasdaq"
+
+        assert dividend_metrics.yield_ == 0.38
+        assert dividend_metrics.pay_out_ratio == 13.16
+        assert dividend_metrics.frequency == "Quarterly"
+        assert dividend_metrics.annual_dividend == Decimal("1.04")
+        assert (
+            dividend_metrics.next_ex_dividend_date
+            == datetime.strptime("2026-05-12", "%Y-%m-%d").date()
+        )
+        assert (
+            dividend_metrics.next_payout_date
+            == datetime.strptime("2026-05-15", "%Y-%m-%d").date()
+        )
+
+        for actual_event, expected_event in zip(
+            dividend_history.dividend_events,
+            _aapl_dividend_history_from_json.dividend_events,
+        ):
+            assert actual_event == expected_event
