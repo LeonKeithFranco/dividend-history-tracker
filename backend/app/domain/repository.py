37a9a731acb9313ffa87c3375024af28a49c6b4c
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-01 10:19:55 -0700

    add get_stock_info to repo

--- backend/app/domain/repository.py
+++ backend/app/domain/repository.py
@@ -1,9 +1,11 @@
 from typing import Annotated
 
+from fastapi import Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from database.db.session import get_db
 from database.models import Stock
-from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession
 
 DbDependency = Annotated[AsyncSession, Depends(get_db)]
 
@@ -12,8 +14,11 @@ class StockRepository:
     def __init__(self, db: DbDependency) -> None:
         self.db: AsyncSession = db
 
-    # async def get_stock_info(self, ticker: str) -> Stock | None:
-    #     query =
+    async def get_stock_info(self, ticker: str) -> Stock | None:
+        query = select(Stock).where(Stock.ticker_symbol == ticker)
+        results = await self.db.execute(query)
+
+        return results.scalar_one_or_none()
 
 
 StockRepoDependency = Annotated[StockRepository, Depends(StockRepository)]
