3040461a32295d8801a1380d61392481bd6df824
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-04-30 17:10:41 -0700

    fix repo file

--- backend/app/domain/repository.py
+++ backend/app/domain/repository.py
@@ -1,21 +1,19 @@
-# from database.models import Stock
-# from typing import Annotated
+from typing import Annotated
 
-# from fastapi import Depends
-# from sqlalchemy.ext.asyncio import AsyncSession
+from database.db.session import get_db
+from database.models import Stock
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
-# from database.db.session import get_db
-
-# DbDependency = Annotated[AsyncSession, Depends(get_db)]
-
-
-# class StockRepository:
-#     def __init__(self, db: DbDependency) -> None:
-#         self.db: AsyncSession = db
+DbDependency = Annotated[AsyncSession, Depends(get_db)]
 
 
-#     async def get_stock_info(self, ticker: str) -> Stock | None:
-#         query =
+class StockRepository:
+    def __init__(self, db: DbDependency) -> None:
+        self.db: AsyncSession = db
+
+    # async def get_stock_info(self, ticker: str) -> Stock | None:
+    #     query =
 
 
-# StockRepoDependency = Annotated[StockRepository, Depends(StockRepository)]
+StockRepoDependency = Annotated[StockRepository, Depends(StockRepository)]
