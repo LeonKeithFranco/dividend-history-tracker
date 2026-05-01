782b017826fa2a6014d7ebd6f1a5f0ecddb8ff5d
Leonard Keith Franco <leonard.k.franco@gmail.com>
2026-05-01 10:27:47 -0700

    typos

--- backend/app/domain/service.py
+++ backend/app/domain/service.py
@@ -1,6 +1,6 @@
 from app.domain.repository import StockRepoDependency, StockRepository
 
 
-class DividendHistorySerive:
+class DividendHistoryService:
     def __init__(self, stock_repo: StockRepoDependency) -> None:
         self.stock_repo: StockRepository = stock_repo
